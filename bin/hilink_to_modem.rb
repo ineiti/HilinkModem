1a3e50fd7799ee6def8edfcb36faf449eacf359a
Linus Gasser <ineiti@linusetviviane.ch>
2014-12-10 09:30:17 +0100

    new files

---
+++ bin/hilink_to_modem.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+LOAD_PATH.push %w( ../lib/ ../../HelperClasses/lib ../../QooxView/libs/activesupport-3.1.1/lib
+../../QooxView/libs/i18n-0.6.0/lib )
+require 'hilinkmodem'
+
+puts HilinkModem::switch_to_modem
+sleep 10
+if File.exists? "/dev/ttyUSB0"
+  puts HilinkModem::Modem::save_modem
+else
+  puts "Switch didn't work - sorry"
+end
